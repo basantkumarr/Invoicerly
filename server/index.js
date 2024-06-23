12fae29118178d42fbe999d4081bf5fb5bc38274
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-23 13:04:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,6 +23,16 @@ app.use(cors({
   credentials: true,
 }));
 
+
+
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+
 // CORS preflight middleware
 app.options('*', cors()); // Enable preflight requests for all routes
 
