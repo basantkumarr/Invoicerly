676f956280a9df18baace4125203b7af95152c0b
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-23 11:38:11 +0530

    Update main.jsx

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -7,13 +7,15 @@ import { ClerkProvider } from '@clerk/clerk-react';
 
 const PUBLISHABLE_KEY = 'pk_test_cHJlcGFyZWQtZm94LTAuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
+console.log('PUBLISHABLE_KEY:', PUBLISHABLE_KEY);
+
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider frontendApi={PUBLISHABLE_KEY}> {/* Correct prop name is frontendApi */}
+    <ClerkProvider frontendApi={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </React.StrictMode>
