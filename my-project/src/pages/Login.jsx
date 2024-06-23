62afdf39d08f47ccf48a1d10f03cdfda7e089806
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-23 10:13:00 +0530

    Update Login.jsx

--- my-project/src/pages/Login.jsx
+++ my-project/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     axios
-      .post("http://localhost:3001/login", {
+      .post("https://invoicerly-server.vercel.app/login", {
         email,
         password,
       })
