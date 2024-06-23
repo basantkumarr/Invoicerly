6471edcc23c361aa168987a683480edda701df1d
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-23 13:06:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,8 +26,8 @@ app.use(cors({
 
 
 app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
+    //Enabling COR
+    res.header("Access-Control-Allow-Origin", "https://invoicerly.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
