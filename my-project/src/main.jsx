c8a5e13da1330c800e5de9775dd567a910893ae4
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-23 11:10:23 +0530

    Update main.jsx

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './App.css';
 
-import { ClerkProvider } from '@clerk/clerk-react'
- 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider frontendAPI={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
