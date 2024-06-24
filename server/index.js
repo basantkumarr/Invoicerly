077db22c3d241977b325524d6c95b467f986fab8
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-24 13:29:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(cors({
   },
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
-  preflightContinue: false,
+  preflightContinue: true,
   optionsSuccessStatus: 204,
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Access-Control-Allow-Origin']
