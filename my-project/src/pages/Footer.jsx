2bb4fc40f4b6b614b02c580fa004e07c87421875
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-10-18 13:49:15 +0530

    Update Footer.jsx

--- my-project/src/pages/Footer.jsx
+++ my-project/src/pages/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
 </svg> InVoicerly</a>
           <p className="text-body-secondary">Phone - 8222864990</p>
-          <p className="text-body-secondary">All the copy rights are reserved © 2024 . Basant Kumar</p>
+          <p className="text-body-secondary">All the copy rights are reserved © InVoicerly </p>
         </div>
 
         <div className="col mb-3"></div>
